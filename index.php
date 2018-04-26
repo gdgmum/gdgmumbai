739148852804fee592fddc7673fdaef0da21c495
gdgmum <38767284+gdgmum@users.noreply.github.com>
2018-04-27 01:06:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,266 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="img/logo1.png">
+
+    <title>GDG Mumbai</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="css/agency.min.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+      <div class="container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top" style="font-family: Product Sans; color: white;">GDG Mumbai</a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          Menu
+          <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav text-uppercase ml-auto">
+            
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#team">Team</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#join">Join us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <!-- Header -->
+    <header class="masthead">
+      <div class="container">
+        <div class="intro-text">
+          <div class="intro-lead-in">  Welcome to </div>
+          <div class="intro-heading text-uppercase">GDG Mumbai</div>
+          <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#about">Tell Me More</a>
+        </div>
+      </div>
+    </header>
+
+   
+
+    
+    <section id="about" style="background-color: #3498db;">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 mx-auto text-center">
+            <h2 class="section-heading text-white">About GDG</h2>
+            <hr class="light my-4">
+            <p class="text-white mb-4" style="font-family: Product Sans; text-align: justify;">GDGs are local groups of developers who are specifically interested in Google products and APIs. Each local group is called a GDG chapter and can host a variety of technical activities for developers - from just a few people getting together to watch our latest videos, to large gatherings with demos and tech talks, to hackathons. However, at the core, GDGs are focused on developers and technical content, and the core audience should be developers.<br><br>
+                GDG Mumbai is an open and non-profit developers group that aims to learn, share and know more about Google technologies. The events organized by this group are absolutely open to newbies, developers, students, lecturers and anyone who is interested in learning, sharing, and discovering Google’s technologies.<br><br>
+
+              GDG is run by passionate individuals in the developer community and is a place to learn about Google Technologies and Tools for developers. Here you can see what local companies and developers are doing with these technologies. It is entirely focused on developers and educational technical content. It is open to the public with a public membership: A place to meet cool and smart people in technology.
+
+           </p>
+            <a class="btn btn-light btn-xl js-scroll-trigger" href="#team">Our Team</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    
+
+    <!-- Team -->
+    <section class="bg-light" id="team">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Mentors of GDG Mumbai</h2>
+            <h3 class="section-subheading text-muted"></h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/1.PNG" alt="">
+              <h4>Mr. Miten Mehta</h4>
+              <!-- <p class="text-muted">Lead Designer</p> -->
+            </div>
+          </div>
+          <div class="col-sm-6">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/2.jpg" alt="">
+              <h4>Mr. Gautam Anand</h4>
+             <!--  <p class="text-muted">Lead Marketer</p> -->
+            </div>
+          </div>
+          <!-- <div class="col-sm-4">
+            <div class="team-member">
+              <img class="mx-auto rounded-circle" src="img/team/3.jpg" alt="">
+              <h4>Diana Pertersen</h4>
+              <p class="text-muted">Lead Developer</p>
+            </div>
+          </div>
+        </div> -->
+        
+      </div>
+    </section>
+
+    <section class="bg-dark text-white" id="join">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading text-uppercase">Join us now !!!</h2>
+            <h3 class="section-subheading text-muted"></h3>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="team-member">
+             <a href="https://www.facebook.com/GoogleDeveloperGroupMumbai/"> <img class="mx-auto rounded-circle" src="img/team/4.jpg" alt="">
+                             
+ <h4>Facebook</h4></a>
+              
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+            <a href="https://www.meetup.com/GDG-Mumbai/">  <img class="mx-auto rounded-circle" src="img/team/5.png" alt="">
+                              
+<h4>Meetup</h4></a>
+              
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="team-member">
+             <a href="https://twitter.com/gdgmumbai"> <img class="mx-auto rounded-circle" src="img/team/6.jpg" alt="">
+              <h4>Twitter</h4></a>
+             
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Clients -->
+   <!--  <section class="py-5">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt="">
+            </a>
+          </div>
+          <div class="col-md-3 col-sm-6">
+            <a href="#">
+              <img class="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt="">
+            </a>
+          </div>
+        </div>
+      </div>
+    </section> -->
+
+    <!-- Contact -->
+    <section id="contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h2 class="section-heading">Let's Get In Touch!</h2>
+            <hr class="my-4">
+            <p class="mb-5">Ready to join us for ultimate experience as a Developer?? Contact Us !</p>
+          </div>
+        </div>
+        <div class="row">
+
+          <div class="col-lg-12 mr-auto text-center">
+            <i class="fa fa-envelope-o fa-3x mb-3 sr-contact"></i>
+           
+
+            <p>
+              <a href="mailto:buzzmdu@gmail.com" style="color: black;">gdgmumbai@gmail.com</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <span class="copyright">Copyright &copy; GDG Mumbai 2018</span>
+          </div>
+          <div class="col-md-4">
+            <ul class="list-inline social-buttons">
+              <li class="list-inline-item">
+                <a href="https://twitter.com/gdgmumbai">
+                  <i class="fa fa-twitter"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.facebook.com/GoogleDeveloperGroupMumbai/">
+                  <i class="fa fa-facebook"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.meetup.com/GDG-Mumbai/">
+                  <i class="fa fa-meetup"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          
+        </div>
+      </div>
+    </footer>
+
+   
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Contact form JavaScript -->
+    <script src="js/jqBootstrapValidation.js"></script>
+    <script src="js/contact_me.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/agency.min.js"></script>
+
+  </body>
+
+</html>
